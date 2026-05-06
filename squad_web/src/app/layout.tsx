80b6e28718cb7a36c8b3fd7593819960aadd5577
uday <udaykiran9147@gmail.com>
2026-05-06 11:50:21 +0530

    feat: initialize root layout and add site icon for application branding

--- squad_web/src/app/layout.tsx
+++ squad_web/src/app/layout.tsx
@@ -29,9 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
-      </head>
       <body>
         {children}
       </body>
